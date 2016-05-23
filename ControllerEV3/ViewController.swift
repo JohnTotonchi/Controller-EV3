cc12b73e3c4d5274cc62fea476a400569acd4feb
Jordy Kingama <jordy.kingama@gmail.com>
2016-05-23 14:36:26 +0200

    Initial Commit

---
+++ ControllerEV3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControllerEV3
+//
+//  Created by Jordy Kingama on 23/05/2016.
+//  Copyright © 2016 La Mixite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
